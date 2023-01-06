562229737d16876a9287373e2fa1c2d74342e12e
Esme Pérez <80126388+EsmePm@users.noreply.github.com>
2023-01-06 13:09:17 -0600

    Add files via upload

--- transform.py
+++ transform.py
@@ -83,4 +83,24 @@ m = map(square,a)
 r = reversed(list(m))
 print(list(r))
 
-#evaluacion perexosa reduce recursos
+#evaluacion perezosa reduce recursos
+
+for i in range(9, -1, -1):
+    print(i)
+
+for i in reversed(range(10)):
+    print(i)
+
+k = [1, 2, 3]
+k.reverse() #reversed retorna un iterable y reverse solo modifica la lista
+print(k)
+
+dates = ['2019/04/06', '2017/02/11', '2010/03/30', '2018/01/01', '2019/04/08', '2017/03/02', '2017/04/05', '2018/06/06', '2019/09/30', '2018/04/11', '2017/03/14']
+sorted_dates = sorted(dates)
+print(sorted_dates)
+
+sorted_dates= sorted(dates, key=lambda x: x[5:7])
+print(sorted_dates)
+
+sorted_dates_by_y = sorted(sorted_dates, key=lambda x: (x[5:7], x[0:4], x[8:10]))
+print(sorted_dates_by_y)
