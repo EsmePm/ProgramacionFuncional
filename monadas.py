0d82bca0176025d7e82401d8f128f02c5bb4421d
Esme Pérez <80126388+EsmePm@users.noreply.github.com>
2023-01-23 13:58:07 -0600

    Add files via upload

--- monadas.py
+++ monadas.py
@@ -1,6 +1,6 @@
 # functor
 #pip install oslash en env
-from oslash import Just
+from oslash import Just, Nothing, List
 from operator import neg
 
 a  = Just(3)
@@ -13,3 +13,16 @@ print(b)
 
 b = neg % a #significa lo mismo que b = a.map(neg)
 print(b)
+
+n = Nothing()
+print(n)
+
+nn = neg % n
+print(nn)
+
+l = List[1, 2, 3]
+print(l)
+print(type(l))
+
+ln = neg % l
+print(ln)
